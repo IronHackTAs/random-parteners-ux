768837e225122fa58f46207f4d18dc5d3d4fab98
Giorgio Grassini <giorgiograssini@gmail.com>
2018-10-08 09:41:49 +0200

    first

--- js/raffle.js
+++ js/raffle.js
@@ -1,42 +1,39 @@
+let i = 0;
+let counter = 0;
 class Raffle {
     constructor(){
+        
+        this.alba =  { name: 'Tata Golosa 👅 (Alba)', img: './img/luis.png',student:0 },            
+        this.luis = { name: 'Luis Pollon 🍆 (Luis)', img: './img/luis.png',student:0 },            
+        this.esther = { name: 'Correa para rato 🐕 (Esther)', img: './img/ester.png',student:0 },            
+        this.jose = { name: 'Rocky Terneras 🥊 (Jose)', img: './img/jose.png',student:0 },       
+
         this.students = _.shuffle([
-            // { name: 'Giorgio', img: 'https://ca.slack-edge.com/T02CQ4EN4-U7XLHB3K8-7a3f2cd06e9a-72 ' },
-            // { name: 'Gabi', img: 'https://ca.slack-edge.com/T02CQ4EN4-U9BLX83D2-204942fa65ea-72' },
-            // { name: 'Juan', img: 'https://ca.slack-edge.com/T02CQ4EN4-U991LCDU4-3fed4d10a310-72' },
-            // { name: 'Susi', img: 'https://ca.slack-edge.com/T02CQ4EN4-U6WSYDE4Q-617818103ce0-72' },
-            // { name: 'Belt', img: 'https://ca.slack-edge.com/T02CQ4EN4-U80LW0H1D-b3e93b28c85d-72' },
-            // { name: 'Marc', img: 'https://ca.slack-edge.com/T02CQ4EN4-U3WM1P708-5739b8c78531-72' },
-            { name: 'Canario🐥 (Adrian)', img: './img/adrian.jpg' },
-            { name: 'Goldhunter (Alfonso)', img: './img/alfonso.jpg' },
-            { name: 'La rizos (Almudena)', img: './img/almudena.jpg' },
-            { name: 'grada #1 /// 円形劇場のリーダー (Celia)', img: './img/celia.jpg' },
-            { name: 'grada #2 (Cesar)', img: './img/cesar.jpg' },
-            { name: 'Con h (Christian)', img: './img/christian.jpg' },
-            { name: 'Sin h (Cristian)', img: './img/cristian.jpg' },
-            { name: 'Transformado de fourier 🤯 (David)', img: './img/david.jpg' },
-            { name: 'La más larga(la coleta) 🍆 (Dennis)', img: './img/denis.jpg' },
-            { name: 'De Pol en Pol (Elvira)', img: './img/elvira.jpg' },
-            { name: 'Esperanzita Rita (Esperanza)', img: './img/esperanza.jpg' },
-            { name: 'Paco Bond 🔫🚁 (Francis)', img: './img/francis.jpg' },
-            { name: 'The Teacher 👨‍🏫 (Gonzalo)', img: './img/gonzalo.jpg' },
-            { name: 'grada #5 (Jacob)', img: './img/jacob.jpg' },
-            { name: '17:00:00 ⏱ (Jhon)', img: './img/jhon.jpg' },
-            { name: 'grada #3 (Laura C.)', img: './img/laura-c.jpg' },
-            { name: 'BOID 🧟‍♀️ (Laura M.)', img: './img/laura-m.jpg' },
-            { name: 'grada #4 (Maria)', img: './img/maria.jpg' },
-            { name: 'Heisenberg 👨🏻‍⚕️ (Mario)', img: './img/mario.jpg' },
-            { name: 'El Catalan 🇵🇱 (Matt)', img: './img/matt.jpg' },
-            { name: 'Jetpack✈️ (Miguel)', img: './img/miguel.jpg' },
-            { name: 'Ludopata ♠️ 🚬 (Pablo)', img: './img/pablo.jpg' },
-            { name: 'El malabarista 🤹🏻‍♂️ (Raul G.)', img: './img/raul-g.jpg' },
-            { name: 'Heisenberg2 👨🏻‍⚕️ (Raul M.)', img: './img/raul-m.jpg' },
-            { name: 'Blockchain (Ricardo)', img: './img/ricardo.jpg' },
-            { name: 'Er Ruben 🚬 (Ruben)', img: './img/ruben.jpg' },
-            { name: 'La peque 👶 (Sara)', img: './img/sara.jpg' },
-            { name: 'Influencer (Teodoro)', img: './img/teodoro.jpg' },
-            { name: 'Czerwony Kapturek 🇪🇨 (Weronika)', img: './img/weronika.jpg' },
-            { name: 'Juan Crush 👪 (Yamilet)', img: './img/yamilet.jpg' },
+            { name: 'Adriana', img: './img/adriana.jpg', ta:[this.luis,this.esther] },
+            { name: 'Alvaro', img: './img/alvaro.jpg', ta:[this.jose,this.esther, this.alba] },
+            { name: 'Andreina', img: './img/andreina.jpg', ta:[this.jose] },
+            { name: 'Beatriz', img: './img/beatriz.jpg', ta:[this.luis,this.alba] },
+            { name: 'Clementina', img: './img/clementina.jpg', ta:[this.jose, this.esther, this.alba] },
+            { name: 'Daniela', img: './img/daniela.jpg', ta:[this.alba,this.esther] },
+            { name: 'Elena', img: './img/elena.jpg', ta:[this.esther,this.alba] },
+            { name: 'Eva C.', img: './img/eva_c.jpg', ta:[this.jose, this.alba] },
+            { name: 'Eva S.', img: './img/eva_s.jpg', ta:[this.esther,this.jose] },
+            { name: 'Gabriela', img: './img/gabriela.jpg', ta:[this.jose, this.alba,this.luis ] },
+            { name: 'Guillermo', img: './img/guillermo.jpg', ta:[this.esther, this.luis] },
+            { name: 'Isabel', img: './img/isabel.jpg', ta:[this.jose, this.esther, this.luis] },
+            { name: 'Javi', img: './img/javi.jpg', ta:[this.esther, this.luis] },
+            { name: 'Karla', img: './img/karla.jpg', ta:[this.alba, this.luis] },
+            { name: 'Macarena', img: './img/macarena.jpg', ta:[this.jose, this.esther, this.luis] },
+            { name: 'Marcos', img: './img/marcos.jpg', ta:[this.jose, this.alba, this.esther] },
+            { name: 'Maria', img: './img/maria.jpg', ta:[this.jose, this.luis] },
+            { name: 'Martin', img: './img/martin.jpg', ta:[this.jose, this.esther] },
+            { name: 'Mercedes', img: './img/mercedes.jpg', ta:[this.jose, this.luis] },
+            { name: 'Nuria', img: './img/nuria.jpg', ta:[this.esther] },
+            { name: 'Pablo R.', img: './img/pablo_r.jpg', ta:[this.alba] },
+            { name: 'Pablo S.', img: './img/pablo_s.jpg', ta:[this.jose, this.esther, this.luis] },
+            { name: 'Pedro', img: './img/pedro.jpg', ta:[this.luis, this.alba] },
+            { name: 'Vero', img: './img/vero_f.jpg', ta:[this.jose] },
+            { name: 'Veronica', img: './img/veronica_s.jpg', ta:[this.jose, this.alba] },
         ]);
         this.students.forEach(e => {
             this.addCard(e);
@@ -49,7 +46,6 @@ class Raffle {
             if(!$(e.currentTarget).hasClass('clicked')){
                 $(e.currentTarget).addClass('clicked');
                 let name = $(e.currentTarget).attr('attr-name');
-                console.log(`Added to pairs ${name}`);
                 this.addPaired(name);
             }
         });
@@ -70,19 +66,26 @@ class Raffle {
     }
 
     addPaired(name){
+        counter++;
+        let {ta} = _.find(this.students, {name})
+        if(counter == 24 && this.jose.student==6){
+            var tas = this.jose;
+            tas.student++
+        }else{
+            var tas = _.minBy(ta, (o) =>  o.student);
+            tas.student++;
+        }
         this.finalPairs.push(name);
+        this.finalPairs.push(tas.name);
         let chunks = _.chunk(this.finalPairs, 2);
         let pairs = $("#pairs");
         pairs.empty();
-        console.log("Rewriting pairs");
-        console.log(chunks);
         chunks.forEach(ch => {
-            console.log("add pair")
             let pair = $(`
                 <div class="pair">
                   <span>${ch[0]}</span>
                    - 
-                  <span>${ch[1] ? ch[1] : '....'}</span>
+                  <span>${ch[1]}</span>
                 </div>
             `);
             pairs.append(pair);
